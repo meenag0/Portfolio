9ac577a07ce222d64e8c3e7b4d3a61a687f12383
meenag0 <139080002+meenag0@users.noreply.github.com>
2024-01-26 04:21:52 -0500

    Updated documentation, fixed cursor bug

--- script.js
+++ script.js
@@ -57,88 +57,115 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
     function AnimatedIntro() {
-        let nameBox = document.createElement("span");
-        nameBox.className = "firstText anim-typewriter";
+
+        // creating span for the 'not moving' and assigning to namebox var
+        let nameBox = document.createElement("span");  
+        nameBox.className = "firstText anim-typewriter";  //setting class of namebox span
         nameBox.id = "nameText";
-        nameBox.innerHTML = "Hi, I am Meena. <br> I am ";
-    
+        nameBox.innerHTML = "Hi, I am Meena. <br> I am ";  
+
+        //creating another span element for combining moving and non-moving string elemtents, and assigning to introText
         var introText = document.createElement("span");
         introText.className = "firstText";
         introText.id = "fullText";
     
-        introText.appendChild(nameBox);
+        introText.appendChild(nameBox); //appening nameBox element to introText container
     
-        document.getElementById('flexibleContainer').appendChild(introText);
+        //appends the introText container (with non-moving text atp to flexibleContainer in HTML (which is just the screen)
+        document.getElementById('flexibleContainer').appendChild(introText); 
+
+        // another span element for moving string elemtents
+        let traitsBox = document.createElement("span");
+        traitsBox.className = "traitsClass";
+        traitsBox.setAttribute('data-type', JSON.stringify(["Creative.", "Passionate.", "A Problem Solver."])); //Holds a JSON string representing an array of strings
+        traitsBox.setAttribute('data-period', '2000');
+        //innerHTML set to non-breaking space: placeholder and will be dynamically updated during the text typing animation
+        traitsBox.innerHTML = "&nbsp;";  
+
+        //appends container for moving text into container for full into text
+        introText.appendChild(traitsBox);
     
-        setTimeout(() => {
-            let traitsBox = document.createElement("span");
-            traitsBox.className = "traitsClass";
-            traitsBox.setAttribute('data-type', JSON.stringify(["Creative.", "Passionate.", "A Problem Solver.", ""]));
-            traitsBox.setAttribute('data-period', '2000');
-            traitsBox.innerHTML = "&nbsp;";
+        //setTimeout introduces a delay, before callback function is executed and nested code is performed iteratively
+        setTimeout(() => {  
     
-            introText.appendChild(traitsBox);
-    
+            // creates cursorTraits span, assign class cursorIcon, appends to introText container 
             let cursorTraits = document.createElement("span");
             cursorTraits.className = "cursorIcon";
             introText.appendChild(cursorTraits);
     
+            //appends updated introText to screen container
             document.getElementById('flexibleContainer').appendChild(introText);
     
-            let TxtTypeTraits = function (el, toRotate, period) {
-                this.toRotate = toRotate;
-                this.el = el;
-                this.loopNum = 0;
-                this.period = parseInt(period, 10) || 2000;
-                this.txt = '';
-                this.tick();
-                this.isDeleting = false;
+            // constructor funct for the 'typewriter' effect ()
+            //el: HTML element to apply effect to, toAnimate: array of strings to display in sequence, period: duration of animation
+            let TxtTypeTraits = function (el, toAnimate, period) {
+                this.toAnimate = toAnimate; //property to store array of chars in strings to display in animation
+                this.el = el; // Stores HTML element to which typewriter effect will be applied
+                this.loopNum = 0; //counter to keep track of current string being displayed
+                this.period = parseInt(period, 10) || 2000; // duration of the animation
+                this.txt = ''; //stores current string being displayed
+                this.tick(); //creates tick method to start animation
+                this.isDeleting = false; //boolean to track if animation is currently deleting characters
             };
     
+            //tick method: creates typewriter animtion and displays updsted text
             TxtTypeTraits.prototype.tick = function () {
-                var i = this.loopNum % this.toRotate.length;
-                var fullTxt = this.toRotate[i];
+
+
+                //this.loopNum: counter that keeps track of the current iteration of the char in string
+                //this.toAnimate: length of the string array
+                //when this.loopNum exceeds length of the array, modulo ensures index wraps around to beginning of array. (cycle through a set of elements repeatedly)
+                let i = this.loopNum % this.toAnimate.length; //calculates remainder when this.loopNum is divided by this.toRotate.length
+                let fullTxt = this.toAnimate[i]; 
     
+                //if chars are being deleted, extract a substring from the beginning of fullTxt up to the length of this.txt minus 1.
                 if (this.isDeleting) {
                     this.txt = fullTxt.substring(0, this.txt.length - 1);
                 } else {
+                    //extracts a substring from the beginning of fullTxt up to the length of this.txt plus 1.
                     this.txt = fullTxt.substring(0, this.txt.length + 1);
                 }
     
-                this.el.innerHTML = this.txt;
-    
-                // Toggle visibility of the cursor based on the current text
+                this.el.innerHTML = this.txt;  //updates inner HTML of element (this.el) with current text.
+
+                // toggle visibility of the cursor based on the current text
                 cursorTraits.style.visibility = this.isDeleting && this.txt === '' ? 'hidden' : 'visible';
     
-                var that = this;
-                var delta = 40 - Math.random() * 20;
+
+                let that = this;  //capturing current value of this in a var
+                let delta = 40 - Math.random() * 20;  //variability in the typing speed
     
+                //if deleting, setting speed
                 if (this.isDeleting) {
                     delta /= 2;
                 }
     
+                //if not deleting, and current text is the end, start deleting
                 if (!this.isDeleting && this.txt === fullTxt) {
                     delta = this.period;
                     this.isDeleting = true;
     
-                    if (i === this.toRotate.length - 1) {
-                        return;
-                    }
+
+                  // if deleting and current text empty, start typing
                 } else if (this.isDeleting && this.txt === '') {
                     this.isDeleting = false;
                     this.loopNum++;
                     delta = 0;
                 }
-    
+
+                //schedules next iteration of animation (tick method) after a delay specified by delta.
                 setTimeout(function () {
                     that.tick();
                 }, delta);
             };
     
-            var toRotate = traitsBox.getAttribute('data-type') || '[]';
+            //Retrieves the attributes 'data-type' and 'data-period' from traitsBox
+            var toAnimate = traitsBox.getAttribute('data-type') || '[]';
             var period = traitsBox.getAttribute('data-period') || '200';
-            new TxtTypeTraits(traitsBox, JSON.parse(toRotate), period);
+            // creates an instance of TxtTypeTraits with these attributes
+            new TxtTypeTraits(traitsBox, JSON.parse(toAnimate), period);
         }, 3500);
+
     }
 
     getDisplayNums();
