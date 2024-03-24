cc826158cb28c97fa9f90a5ac49d976fe5732cc1
meenag0 <139080002+meenag0@users.noreply.github.com>
2024-03-24 02:48:49 -0400

    added blurred navbar, but changin it to solid

--- script.js
+++ script.js
@@ -1,75 +1,41 @@
 document.addEventListener('DOMContentLoaded', function () {
-  console.log("DOM content loaded");
+    console.log("DOM content loaded");
 
+    const typedTextSpan = document.querySelector(".traitsClass"); // Change to class selector
+    const cursorSpan = document.querySelector(".cursorIcon"); // Change to class selector
 
-  function AnimatedIntro() {
-      // assuming traitsBox already exists in your HTML
-      let traitsBox = document.querySelector("#traitsBox");
-      console.log("traitsBox:", traitsBox); // Log the result of querySelector
+    const textArray = ["Creative", "Passionate", "Inquisitive"];
+    const typingDelay = 150;
+    const erasingDelay = 100;
+    const newTextDelay = 2000;
+    let textArrayIndex = 0;
+    let charIndex = 0;
 
-      let words = traitsBox.querySelectorAll(".word");
-      console.log("Words inside traitsBox:", words); // Log the elements with class '.word'
-      let wordArray = [];
-      words.forEach(function(word) {
-          wordArray.push(word.innerHTML);
-      });
-      // traitsBox.innerHTML = ""; // Clearing the content of traitsBox
+    function type() {
+        if (charIndex < textArray[textArrayIndex].length) {
+            if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+            typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+            charIndex++;
+            setTimeout(type, typingDelay);
+        } else {
+            cursorSpan.classList.remove("typing");
+            setTimeout(erase, newTextDelay);
+        }
+    }
 
-      // set timeout to initiate typewriter effect after a delay
-      setTimeout(() => {
-          // constructor function for the 'typewriter' effect
-          let TxtTypeTraits = function (el, toAnimate, period) {
-              this.toAnimate = toAnimate;
-              this.el = el;
-              this.loopNum = 0;
-              this.period = parseInt(period, 10) || 2000;
-              this.txt = '';
-              this.tick();
-              this.isDeleting = false;
-          };
+    function erase() {
+        if (charIndex > 0) {
+            if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+            typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex - 1);
+            charIndex--;
+            setTimeout(erase, erasingDelay);
+        } else {
+            cursorSpan.classList.remove("typing");
+            textArrayIndex++;
+            if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+            setTimeout(type, typingDelay + 1100);
+        }
+    }
 
-          // tick method for typewriter animation
-          TxtTypeTraits.prototype.tick = function () {
-              let i = this.loopNum % this.toAnimate.length;
-              let fullTxt = this.toAnimate[i];
-
-              if (this.isDeleting) {
-                  this.txt = fullTxt.substring(0, this.txt.length - 1);
-              } else {
-                  this.txt = fullTxt.substring(0, this.txt.length + 1);
-              }
-
-              this.el.innerHTML = this.txt;
-
-              let that = this;
-              let delta = 300 - Math.random() * 20;
-
-              if (this.isDeleting) {
-                  delta /= 1.5;
-              }
-
-              if (!this.isDeleting && this.txt === fullTxt) {
-                  delta = this.period;
-                  this.isDeleting = true;
-              } else if (this.isDeleting && this.txt === '') {
-                  this.isDeleting = false;
-                  this.loopNum++;
-                  delta = 0;
-              }
-
-              setTimeout(function () {
-                  that.tick();
-              }, delta);
-          };
-
-          // create an instance of TxtTypeTraits with words array
-          new TxtTypeTraits(traitsBox, wordArray, 200);
-      }, 500);
-  }
-
-
-  // execute AnimatedIntro
-  AnimatedIntro();
-
-
+    if (textArray.length) setTimeout(type, newTextDelay + 250);
 });
