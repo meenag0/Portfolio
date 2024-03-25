23297e63ee7a517b5ff4b5b5a7f343b622d64d1c
meenag0 <139080002+meenag0@users.noreply.github.com>
2024-03-24 20:27:45 -0400

    added basic nav bar for projects

--- script.js
+++ script.js
@@ -38,4 +38,35 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     if (textArray.length) setTimeout(type, newTextDelay + 250);
+
+    const getItemOffset = (item) => {
+        return item.offsetTop;
+      };
+      
+      const moveMarker = (offset) => {
+        const marker = document.querySelector('.active-marker');
+        marker.style.transform = `translateY(${offset}px)`;
+        
+      };
+      
+      const toggleActive = (e) => {
+        e.preventDefault();
+        
+        // Remove any existing active classes
+        const links = document.querySelectorAll('.vertical-menu-item');
+        links.forEach((link => link.classList.remove('is-active')));
+        
+        // Add class to active link
+        const activeItem = e.target.parentElement
+        activeItem.classList.toggle('is-active');
+        const offset = getItemOffset(activeItem);
+        moveMarker(offset);
+      };
+      
+      // Attach click event listener
+      const menu = document.querySelector('.vertical-menu');
+      
+      menu.addEventListener('click', toggleActive);
+      
+      
 });
