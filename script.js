4789153d24ca2c62e5210d68f83436c557e734f4
meenag0 <139080002+meenag0@users.noreply.github.com>
2024-04-04 23:04:15 -0400

    changed design

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+
 document.addEventListener('DOMContentLoaded', function () {
     console.log("DOM content loaded");
+
 
     const typedTextSpan = document.querySelector(".traitsClass"); // Change to class selector
     const cursorSpan = document.querySelector(".cursorIcon"); // Change to class selector
@@ -39,34 +41,4 @@ document.addEventListener('DOMContentLoaded', function () {
 
     if (textArray.length) setTimeout(type, newTextDelay + 250);
 
-    const getItemOffset = (item) => {
-        return item.offsetTop;
-      };
-      
-      const moveMarker = (offset) => {
-        const marker = document.querySelector('.active-marker');
-        marker.style.transform = `translateY(${offset}px)`;
-        
-      };
-      
-      const toggleActive = (e) => {
-        e.preventDefault();
-        
-        // Remove any existing active classes
-        const links = document.querySelectorAll('.vertical-menu-item');
-        links.forEach((link => link.classList.remove('is-active')));
-        
-        // Add class to active link
-        const activeItem = e.target.parentElement
-        activeItem.classList.toggle('is-active');
-        const offset = getItemOffset(activeItem);
-        moveMarker(offset);
-      };
-      
-      // Attach click event listener
-      const menu = document.querySelector('.vertical-menu');
-      
-      menu.addEventListener('click', toggleActive);
-      
-      
 });
