d0ea9a9c0f89814fa7a0a973fbf3cbdd3f7929d0
meenag0 <139080002+meenag0@users.noreply.github.com>
2024-04-21 04:25:33 -0400

    added config

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,15 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: 'Portfolio', // Replace 'your-root-directory' with the directory containing your project
-  base: '/', // Replace 'your-base-url' with the base URL of your project
+  base: '/', 
+  build: {
+    outDir: 'dist',
+    assetsDir: '',
+    rollupOptions: {
+      input: {
+        // Change this to point to your entry JavaScript file
+        main: 'script.js'
+      }
+    }
+  }// Replace 'your-base-url' with the base URL of your project
 });
